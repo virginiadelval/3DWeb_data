fc7807cff2b95a29f548d264c46593f1d01f3660
Virginia Del Val <vdelval@salta02.local>
2026-06-09 12:28:27 -0300

    revert: change active parcel highlight style back to original light blue

--- source/src/components/Parcel/Polygon.js
+++ source/src/components/Parcel/Polygon.js
@@ -5,23 +5,12 @@ import { getParcelLayer } from 'utils/configQueries'
 import MapaInteractivoGL from 'utils/MapaInteractivoGL'
 import IFC from 'components/IFC'
 const parcelId = 'parcel_layer'
-// Añadir pleta de colores//
-function styleCatastros(feature) {
-  return {
-    fillColor: '#000000',
-    weight: 1.5,
-    opacity: 0.7,
-    color: 'black',
-    dashArray: '0',
-    fillOpacity: 0
-  }
-}
-
 const Polygon = ({ smpList, geomCoords }) => {
   const mapGL = MapaInteractivoGL()
 
   const {
-    edif
+    edif,
+    polygon: { paint, type, layout }
   } = getParcelLayer()
   const { id: edifId } = edif
   useEffect(() => {
@@ -58,49 +47,20 @@ const Polygon = ({ smpList, geomCoords }) => {
           }
         })
       }
-      
-      const style = styleCatastros({ geometry: geomCoords })
-
-      // Capa de relleno (sin color adentro, opacidad 0)
       mapGL.map.addLayer({
         id: parcelId,
         source: parcelId,
-        type: 'fill',
-        paint: {
-          'fill-color': style.fillColor,
-          'fill-opacity': style.fillOpacity
-        }
+        type,
+        layout,
+        paint
       })
-
-      // Capa de borde de color negro
-      const borderLayerId = `${parcelId}_border`
-      mapGL.map.addLayer({
-        id: borderLayerId,
-        source: parcelId,
-        type: 'line',
-        paint: {
-          'line-color': style.color,
-          'line-width': style.weight,
-          'line-opacity': style.opacity
-        }
-      })
-
       mapGL.map.moveLayer(parcelId, edifId)
-      mapGL.map.moveLayer(borderLayerId, edifId)
     }
     return () => {
-      if (mapGL.map.getLayer(parcelId)) {
-        mapGL.map.removeLayer(parcelId)
-      }
-      const borderLayerId = `${parcelId}_border`
-      if (mapGL.map.getLayer(borderLayerId)) {
-        mapGL.map.removeLayer(borderLayerId)
-      }
-      if (mapGL.map.getSource(parcelId)) {
-        mapGL.map.removeSource(parcelId)
-      }
+      mapGL.map.removeLayer(parcelId)
+      mapGL.map.removeSource(parcelId)
     }
-  }, [geomCoords, mapGL, edifId])
+  }, [geomCoords, mapGL, paint, type, layout, edifId])
 
   return <IFC />
 }
